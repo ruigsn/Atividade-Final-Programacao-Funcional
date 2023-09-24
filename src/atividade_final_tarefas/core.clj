3099244a1db4f2e7342954332a603ac5c4e7ab82
Rui de Gouveia Soares Neto <ruigsn@edu.unifor.br>
2023-09-24 13:28:36 -0300

    Documentação

--- src/atividade_final_tarefas/core.clj
+++ src/atividade_final_tarefas/core.clj
@@ -7,16 +7,19 @@
   (read-line))
 
 (defn adicionar-tarefa [descricao]
+  ;aqui temos o primeiro exemplo de "Closure" no programa, pois a função adicionar-tarefa faz referência à variável lista-tarefas, que é uma variável definida em uma função externa de onde adicionar-tarefa foi definida.
   (when (not (empty? descricao)) ; Verifica se a descrição não está vazia
     (let [id (if (empty? @lista-tarefas)
                1
                (->> @lista-tarefas (map :id) (apply max) inc)) ;Uso da função "map", em Clojure, é equivalente à List Comprehension, pois para cada valor de uma lista, você aplica uma operação e coloca todos os resultados em uma nova lista.
-          nova-tarefa {:id id :descricao descricao}]
+          nova-tarefa {:id id :descricao descricao}
+          ;A função interna nova-tarefa também é uma closure, pois faz referência à variável lista-tarefas, definida em uma função externa.
+          ]
       (swap! lista-tarefas conj nova-tarefa))))
 
 (defn excluir-tarefa [id]
   (swap! lista-tarefas (fn [tarefas] (remove #(= id (:id %)) tarefas))))
-;; Na função "excluir-tarefa", aplicamos a função swap ao atom "lista-tarefas", mas precisamos passar outra função como parâmetro. Para não termos que criar outra função, usamos a função anônima, também conhecida em outras linguagens como lambda. Podemos observar o uso em clojure através da "fn" recebendo o parâmetro tarefas e outra função anônima para captarmos o indicador da lista conforme o valor escolhido.
+;; Na função "excluir-tarefa", aplicamos a função swap ao atom "lista-tarefas", mas precisamos passar outra função como parâmetro. Para não termos que criar outra função, usamos a função anônima, também conhecida em outras linguagens como lambda. Podemos observar o uso em clojure através da "fn" recebendo o parâmetro tarefas e outra função anônima, caracterizada com o "#" para captarmos o indicador da lista conforme o valor escolhido.
 
 (defn listar-tarefas []
   (if (empty? @lista-tarefas)
