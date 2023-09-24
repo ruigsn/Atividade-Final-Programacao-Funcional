50f040f4320d819b2e8e66db6ec073c44ee4708b
Rui de Gouveia Soares Neto <ruigsn@edu.unifor.br>
2023-09-24 13:40:36 -0300

    Documentação

--- src/atividade_final_tarefas/core.clj
+++ src/atividade_final_tarefas/core.clj
@@ -1,6 +1,7 @@
 (ns atividade-final-tarefas.core)
 
 (def lista-tarefas (atom []))
+;; Os "Átomos" (atom) são um tipo de dados no Clojure que fornecem uma maneira de gerenciar estados compartilhados, síncronos e independentes. Um átomo é como qualquer tipo de referência em qualquer outra linguagem de programação. O principal uso de um átomo é manter as estruturas de dados imutáveis do Clojure.
 
 (defn obter-descricao-tarefa []
   (println "Digite a descricao da tarefa:")
@@ -16,10 +17,12 @@
           ;A função interna nova-tarefa também é uma closure, pois faz referência à variável lista-tarefas, definida em uma função externa.
           ]
       (swap! lista-tarefas conj nova-tarefa))))
+;; Uma função de alta ordem é aquela que aceita uma ou mais funções como argumentos e/ou retorna uma função como resultado. A função swap! é uma função de alta ordem em Clojure, pois ela aceita uma função como argumento e a aplica no valor do átomo. Neste caso, a função conj é passada como um argumento para swap!, e ela é aplicada no valor atual do átomo lista-tarefas.
 
 (defn excluir-tarefa [id]
   (swap! lista-tarefas (fn [tarefas] (remove #(= id (:id %)) tarefas))))
 ;; Na função "excluir-tarefa", aplicamos a função swap ao atom "lista-tarefas", mas precisamos passar outra função como parâmetro. Para não termos que criar outra função, usamos a função anônima, também conhecida em outras linguagens como lambda. Podemos observar o uso em clojure através da "fn" recebendo o parâmetro tarefas e outra função anônima, caracterizada com o "#" para captarmos o indicador da lista conforme o valor escolhido.
+;; Também podemos perceber que a função remove também é outra função de alta ordem. Ela também aceita uma função como argumento. Aqui, a função anônima #(= id (:id %)) é passada como argumento para remove, indicando a condição de remoção dos elementos da sequência tarefas.
 
 (defn listar-tarefas []
   (if (empty? @lista-tarefas)
